51427050a8bda799cda1b0de3a6e837f2619e693
Veronique-Tritsch <veronique.tritsch@gmail.com>
2021-12-01 11:38:12 +0100

    Ajout de l'exercice Moyenne en java

---
+++ eclipse-workspace/ExoMoyenne/src/Moyenne.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Moyenne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Traduction du pseudo code en code Java
+		
+		//DECLARATION DES VARIABLES
+		//Réel nombre1
+		float nombre1;
+		//Réel nombre2
+		float nombre2;
+		// Réel moyenne
+		float moyenne;
+
+		//SAISIE
+		//écrire "Donnez-moi le premier nombre :"
+		System.out.println("Donnez-moi svp le premier nombre :");
+		//lire nombre1
+		Scanner sc = new Scanner(System.in); //créer un outil scanner nommé sc
+		nombre1 = sc.nextFloat();
+		//System.out.println ("Vous avez saisi nombre1 :"+nombre1);
+		
+		System.out.println ("Donnez-moi svp le deuxième nombre :");
+		nombre2 = sc.nextFloat();
+		//System.out.println ("Vous avez saisi nombre2 :"+nombre2);
+		
+		//TRAITEMENT
+		//calcul de la moyenne
+		moyenne = (nombre1+nombre2)/2;
+		
+		//AFFICHAGE
+		//ecrire "La moyenne des deux nombres est :",moyenne
+		System.out.println ("La moyenne des deux nombres est: "+moyenne);
+		
+	}
+
+}
