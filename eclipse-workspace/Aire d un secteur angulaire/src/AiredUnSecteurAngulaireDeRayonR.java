26c33bf818274064e3fc59d97ec6ac0546c864e7
Veronique-Tritsch <veronique.tritsch@gmail.com>
2021-12-06 12:39:37 +0100

    Java : aire d'un secteur angulaire

---
+++ eclipse-workspace/Aire d un secteur angulaire/src/AiredUnSecteurAngulaireDeRayonR.java
@@ -0,0 +1,14 @@
+
+public class AiredUnSecteurAngulaireDeRayonR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//DECLARATION DES VARIABLES
+		float rayon
+		float angle
+		double AireDuSecteurAngulaire
+		w<q
+		}
+
+}
