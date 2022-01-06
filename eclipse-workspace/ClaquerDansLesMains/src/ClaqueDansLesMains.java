6a867861065ddf1137e13e89233c72dd537b582a
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-01-06 13:10:26 +0100

    Claque dans les mains en Java

---
+++ eclipse-workspace/ClaquerDansLesMains/src/ClaqueDansLesMains.java
@@ -0,0 +1,12 @@
+
+public class ClaqueDansLesMains {
+
+	public static void main(String[] args) {
+		int i ;
+		for( i=1 ; i<=6 ; i++ ) {
+			System.out.println("Claquez dans les mains pour la "+i+"ème fois");	
+
+		}
+		System.out.println("Je suis sorti(e)");
+	} 
+}
