23267e12c461ab7fa08a1b6cdf8fe1c068afae5d
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-01-06 15:01:20 +0100

    Claquer dans les mains en Java
    Dernière mouture

--- eclipse-workspace/ClaquerDansLesMains/src/ClaqueDansLesMains.java
+++ eclipse-workspace/ClaquerDansLesMains/src/ClaqueDansLesMains.java
@@ -3,10 +3,11 @@ public class ClaqueDansLesMains {
 
 	public static void main(String[] args) {
 		int i ;
+		
 		for( i=1 ; i<=6 ; i++ ) {
 			System.out.println("Claquez dans les mains pour la "+i+"ème fois");	
-
-		}
+			}
+		
 		System.out.println("Je suis sorti(e)");
 	} 
 }
