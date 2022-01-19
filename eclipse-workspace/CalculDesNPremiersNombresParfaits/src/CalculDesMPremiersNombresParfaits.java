72160cd36f0baf522e8425d0d8519eefab767ce6
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-01-19 13:18:08 +0100

    Détermination des N premiers nb parfaits en Java
    Bien nommer les variables pour s'y retrouver et bien comprendre à la lecture du programme en Java

---
+++ eclipse-workspace/CalculDesNPremiersNombresParfaits/src/CalculDesMPremiersNombresParfaits.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CalculDesMPremiersNombresParfaits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nbDeNbParfaitsATrouver ;
+		int nbCourantDeNbParfaits ;
+		int nbParfaitPotentiel ;
+		int diviseur ;
+
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("Entrer un nombre entier");
+				nbDeNbParfaitsATrouver = sc.nextInt() ;
+		
+		nbCourantDeNbParfaits = 1 ;
+		
+		nbParfaitPotentiel = 2 ;
+		
+		while (nbCourantDeNbParfaits <= nbDeNbParfaitsATrouver) {
+			int somme=0 ;
+			for (diviseur =1 ; diviseur < nbParfaitPotentiel-1 ; diviseur++)	
+			{
+				if(nbParfaitPotentiel % diviseur == 0) {
+					somme = somme + diviseur ;	
+				}
+			}
+			if (somme==nbParfaitPotentiel) {
+				nbCourantDeNbParfaits = nbCourantDeNbParfaits + 1 ;
+				System.out.println(nbParfaitPotentiel+" est parfait");
+			}
+
+			nbParfaitPotentiel = nbParfaitPotentiel+1 ;	
+		}
+			
+		sc.close() ;
+	}	
+}
+
+
