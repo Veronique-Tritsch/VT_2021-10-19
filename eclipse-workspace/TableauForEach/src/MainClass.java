9194c163016c69fde36afe7cce82073ae46f9139
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-02-23 15:49:14 +0100

    Présentation d'une boucle for each dans un tableau de chaînes caractères
    Riri, Fifi et Loulou sont écrits à la suite par la console

---
+++ eclipse-workspace/TableauForEach/src/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+	public static void main(String[] args) {
+		String[] tableau = {"Riri","Fifi","Loulou"};
+		for (String s : tableau)
+			System.out.println(s);
+		
+	}
+}
