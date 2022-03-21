4d18a52f1424d226977c7e63691db6a77e18e5da
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-03-21 16:09:08 +0100

    Jeu du pendu en Java

--- eclipse-workspace/JeuDuPendu/src/JouonsAuPendu.java
+++ eclipse-workspace/JeuDuPendu/src/JouonsAuPendu.java
@@ -6,7 +6,7 @@ public class JouonsAuPendu {
 		// TODO Auto-generated method stub
 
 		String mot ; 
-		char[] tableauDeCaracteres = new char[15] ;
+		
 		char caractere ;
 		String letter ;
 		int indice ;
@@ -17,12 +17,25 @@ public class JouonsAuPendu {
 
 		do {
 			System.out.println("Veuillez saisir un mot d'au moins 5 lettres");
-			mot = sca.nextLine() ;
+			/* Pour éviter les problèmes de casse, on récupère la chaîne de caractères mot en majuscules. 
+			 * On ne comparera par après que des lettres en majuscules
+			 */
+			mot = sca.nextLine().toUpperCase() ;
 		}
 		while (mot.length() < 5) ;
-
+		/* On déclare "TableauDeCaracteres" qui est un tableau de caractères 
+		 * constitué de mot.length() caractères
+		 */
+		char[] tableauDeCaracteres = new char[mot.length()] ;
+		/* On affecte au premier et dernier terme de "tableauDeCaracteres" 
+		 * respectivement la première et la dernière lettre du de la chaîne de caractères "mot"
+		 * et aux autres termes on affecte un tiret
+		 */
 		tableauDeCaracteres[0] = mot.charAt(0);
 		tableauDeCaracteres[mot.length()-1] = mot.charAt(mot.length()-1) ;
+		/* On affecte aux termes de tableauDeCaracteres 
+		 * qui ne sont pas aux extrémités la valeur "-" 
+		 */
 		for (int i = 1 ; i < mot.length()-1 ; i++) {
 			tableauDeCaracteres[i] = '-' ; }
 
@@ -32,9 +45,16 @@ public class JouonsAuPendu {
 			System.out.print(tableauDeCaracteres[i]) ; }
 
 		indice = 0 ;
+		String valeurActuelle;
 		do {
+			
 			System.out.println("\nproposez un caractère (ou une lettre)") ;
-			letter = sca.next() ;
+			/* il y a un "if ternaire" dans l'instruction qui suit (après le mot "essai") :
+			 * on mettra un "s" à "essai" si le nombre (6-indice) est supérieur à 1 */
+			System.out.println("vous avez encore droit à " + (6-indice) +" essai"+((6-indice) > 1 ? "s":""));
+			/* Pour éviter des casses différentes lors des comparaisons 
+			 * on récupère la chaîne de caractères string en majuscules. */
+			letter = sca.next().toUpperCase() ;
 			caractere = letter.charAt(0) ;
 			caractereTrouve = false ;
 
@@ -45,10 +65,17 @@ public class JouonsAuPendu {
 				if (caractereTrouve == false && j == mot.length()-2)
 				{ indice++ ;}
 			}
+			for (int i = 0 ; i< mot.length() ; i++) {
+				System.out.print(tableauDeCaracteres[i]) ; }
+			/*copyValueOf est une fonction qui convertit 
+			un tableau de caractères en chaîne de caractères
+			*/
+			valeurActuelle = String.copyValueOf(tableauDeCaracteres);
+		} while ((indice < 6 || caractereTrouve == true) && !mot.equals(valeurActuelle)) ;
+
+		if (mot.equals(valeurActuelle)) {
+			System.out.println("le mot " + valeurActuelle + " a été trouvé");
 		}
-		while (indice < 6 || caractereTrouve == true) ;
-
-
 
 
 
