0cf6db2d1831497940faceeef6b735b7312604a1
Veronique-Tritsch <veronique.tritsch@gmail.com>
2022-03-21 16:08:40 +0100

    Héritage en Java

---
+++ eclipse-workspace/Heritage/src/Animal.java
@@ -0,0 +1,16 @@
+
+public class Animal {
+
+	protected String espece ;
+	
+	public String getEspece() 
+	{
+		return this.espece.toUpperCase();
+	}
+	
+	public void setEspece(String nouvelleEspece) 
+	{
+		this.espece = nouvelleEspece;
+	}
+
+}
---
+++ eclipse-workspace/Heritage/src/Programme.java
@@ -0,0 +1,21 @@
+
+public class Programme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		Animal toto;
+		Animal tata;
+		
+		toto = new Animal();
+		
+		tata = new Animal();
+		
+		tata.setEspece("chat");
+		System.out.println("toto est un "+ toto.getEspece());
+		
+
+	}
+
+}
